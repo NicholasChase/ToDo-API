fc173c91f451472791ad954c037744b204bbfd3b
NicholasChase <49351249+NicholasChase@users.noreply.github.com>
2022-02-15 11:18:56 -0500

    refined alg

--- todo/src/repo/repo.ts
+++ todo/src/repo/repo.ts
@@ -1,6 +1,6 @@
 import { Task } from "../todo";
 
-export interface reposotoryPattern<T> {
+interface reposotoryPattern<T> {
     find(findTodo: String),
     findAll(),
     create(makeATodo: T), 
@@ -15,81 +15,90 @@ class TodoRepo implements reposotoryPattern<Task> {
 
     private task: Task[] = [];
 
-    // Get all tasks
+    // GET all tasks
     public findAll() {
         return this.task
     }
 
-    // Post
+    // POST
     public create(newTask) {
         this.task.push(newTask);
         return newTask
     }
 
-    // Get a single task
+    // GET a single task
     public find(uuid) {
-        
-        const found = this.task.some(tasks => tasks.uuid == uuid);
-                if(found) {
-                    return(this.task.filter(tasks => tasks.uuid == uuid));
-                } else {
-                    return (`No task with the ID of ${uuid}`)
-                } 
-    }
-
-    // Delete 
-    public del(uuid) {
-        const found = this.task.some(tasks => tasks.uuid == uuid);
-            if(found) {
-                this.task.map(tasks => {
-                    if (tasks.uuid == uuid) {
-                        this.task.splice(this.task.indexOf(tasks), 1);
-                    }  
-                });
-                return(this.task.filter(task => task.uuid !== parseInt(uuid)));
-            } else {
-                return (`No task with the ID of ${uuid}`)
-            }
-    }
-
-    // Update
-    public updateTodo(uuid, payload) {
-        const found = this.task.some(tasks => tasks.uuid == uuid);
-        if(found){
-            this.task.map(tasks => {
-                if (tasks.uuid == uuid) {
-                    tasks.todo = payload['todo'];
-                    tasks.dueDate = payload['dueDate'];
-                    tasks.complete = payload['completed'];
-                }
-            })
-            return this.task.find(task => task.uuid == uuid)
+        const found = this.task.filter(tasks => tasks.uuid == uuid)
+        if(found) {
+            return(found);
         } else {
             return (`No task with the ID of ${uuid}`)
-        }
+        } 
     }
 
-    // incomplete status
+    // DEL
+    public del(uuid) {
+        //Find element by uuid
+        const result = this.task.filter(tasks => tasks.uuid == uuid);
+        
+        if(result.length === 0) {
+            return `${uuid} does not exist`
+        }
+
+        const element = result[0]
+
+        // find index of uuid
+        const index = this.task.indexOf(element)
+
+        if (index < 0) {
+            return
+        }
+
+        // splice the array at the indexOf
+        this.task.splice(index, 1);
+        return this.task
+
+    }
+
+    // Update PUT
+    public updateTodo(uuid, payload) {
+        const updateTodo = this.task.filter(tasks => tasks.uuid == uuid);
+
+        if(updateTodo.length === 0) {
+            return `${uuid} does not exist`;
+        }
+        this.task.forEach(tasks => {
+            if (tasks.uuid == uuid) {
+                tasks.todo = payload['todo'];
+                tasks.dueDate = payload['dueDate'];
+                tasks.complete = payload['completed'];
+            }
+        }) 
+        return updateTodo
+    }
+
+    // GET incomplete status
     public incompleteTodos() {
-        const found = this.task.some(task => false === task.complete);
-        if(found) {
-            return(this.task.filter(task => false === task.complete))
+        const incompleteTodos = this.task.filter(task => false === task.complete);
+        if(incompleteTodos) {
+            return(incompleteTodos);
         } else {
             return (`No todo is completed`);
         }
     }
 
-    // incomplete status
+    // GET complete status
     public completeTodos() {
-        const found = this.task.some(task => true === task.complete);
-        if(found) {
-            return(this.task.filter(task => true === task.complete))
+        // const found = this.task.some(task => true === task.complete);
+        const completedTodo = this.task.filter(task => true === task.complete)
+        if(completedTodo) {
+            return(completedTodo)
         } else {
             return (`No todo is completed`);
         }
     }
 
-    // order the todos by date
+    // GET order the todos by date
     public orderDates() {
         let sortedTasks = this.task.sort((a, b) => a.dueDate.getTime() - b.dueDate.getTime());
         return sortedTasks
